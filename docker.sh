755a77774014859c66a299aa6c32848b4ead47ef
Adam Dyson <contact@adamdyson.com.au>
2017-11-22 15:51:58 +1100

    Issues reading ENV variable.

--- docker.sh
+++ docker.sh
@@ -77,12 +77,12 @@ build_action()
 }
 
 # If an environment variable exists with an Ansible Vault password.
-if [ -z "${ANSIBLE_VAULT_PASSWORD}" ]; then
+if [ -z "${!ANSIBLE_VAULT_PASSWORD}" ]; then
   # Set the file path that will contain the password.
   printf "\n${heading}Creating Ansible Vault password file.${neutral}\n"
   password_file="/etc/ansible/playbook_under_test/password.txt"
   # Output the password into the password file.
-  docker exec --tty ${container_id} env TERM=xterm bash -c "touch ${password_file} && echo '${ANSIBLE_VAULT_PASSWORD}' >> ${password_file}"
+  docker exec --tty ${container_id} env TERM=xterm bash -c "touch ${password_file} && echo '${!ANSIBLE_VAULT_PASSWORD}' >> ${password_file}"
 fi
 
 # Test the role
